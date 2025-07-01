2a4852b23607ba55a5f7d7b325ea6a6b69127d18
dnl312 <danielpj1234@gmail.com>
2025-07-01 18:32:35 +0700

    done soal 1

--- 1_pair_elements/main.go
+++ 1_pair_elements/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func minPairSum(nums []int) int {
+	sort.Ints(nums)
+	sum := 0
+	for i := 0; i < len(nums); i += 2 {
+		sum += nums[i]
+	}
+	return sum
+}
+
+func main() {
+	arr := []int{1, 4, 3, 2, 5, 6}
+	result := minPairSum(arr)
+	fmt.Println("Jumlah pasangan terkecil maksimal:", result)
+}
